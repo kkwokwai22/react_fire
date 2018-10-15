6472df1157bcb37143f0247d7b8213679c8ee87e
kkwokwai22 <kkwokwai22@gmail.com>
2018-10-15 12:20:58 -0700

    add signIn

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        <form className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign In</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
